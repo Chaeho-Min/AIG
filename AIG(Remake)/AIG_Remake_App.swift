88d49482f346efe11eccb09c46ed04231eafe057
Chaeho-Min <75792767+Chaeho-Min@users.noreply.github.com>
2022-05-05 16:50:54 +0900

    Initial Commit

---
+++ AIG(Remake)/AIG_Remake_App.swift
@@ -0,0 +1,17 @@
+//
+//  AIG_Remake_App.swift
+//  AIG(Remake)
+//
+//  Created by 민채호 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct AIG_Remake_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
