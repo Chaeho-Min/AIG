9d41a3c6919cc12b7c7c68e9950dda231bee9aca
Chaeho-Min <75792767+Chaeho-Min@users.noreply.github.com>
2022-05-06 01:55:58 +0900

    진짜 최종

--- AIG(Remake)/Model/GuideData.swift
+++ AIG(Remake)/Model/GuideData.swift
@@ -6,25 +6,12 @@
 //
 
 import Foundation
-import SwiftUI
 
 struct Guide {
     
     let guideTitle: String
     let guideDescription: String
 }
-
-//let guide1 = "Gesture"
-//let guide2 = "Expect"
-//let guide3 = "Identifiable"
-//let guide4 = "Minimal"
-//
-//let guide1Description = "Never Only"
-//let guide2Description = "Behaviors In Ways People"
-//let guide3Description = "Features Must Be"
-//let guide4Description = "Keep It"
-//
-//let guide1Question = "제스처로만 사용 가능한 기능이 있는가?"
 
 let guideData: [Guide] = [
     Guide(guideTitle: "Gesture", guideDescription: "Never Only"),
